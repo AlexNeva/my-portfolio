925181fb61157c8408d24c983d4c88fffef98f76
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-04-12 19:47:50 +0300

    add header and profile

--- frontend_react/src/components/my-profile/advantages/Advantages.jsx
+++ frontend_react/src/components/my-profile/advantages/Advantages.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
+import classes from './Advantages.module.scss';
 
 const Advantages = () => {
   return (
-    <div className="profile__info grid">
-      <div className="profile__info-group">
-        <h3 className="profile__info-number">2</h3>
-        <p className="profile__info-description">
+    <div className={`${classes.ProfileInfo} grid`}>
+      <div className={classes.InfoGroup}>
+        <h3 className={classes.InfoNumber}>2</h3>
+        <p className={classes.InfoDescr}>
           Years of <br /> work
         </p>
       </div>
-      <div className="profile__info-group">
-        <h3 className="profile__info-number">+100</h3>
-        <p className="profile__info-description">
+      <div className={classes.InfoGroup}>
+        <h3 className={classes.InfoNumber}>+100</h3>
+        <p className={classes.InfoDescr}>
           Completed <br /> projects
         </p>
       </div>
-      <div className="profile__info-group">
-        <h3 className="profile__info-number">+20</h3>
-        <p className="profile__info-description">Satisfied <br /> customers</p>
+      <div className={classes.InfoGroup}>
+        <h3 className={classes.InfoNumber}>+20</h3>
+        <p className={classes.InfoDescr}>Satisfied <br /> customers</p>
       </div>
     </div>
   )
